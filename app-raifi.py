9c2bcc14c6d6917a8840f2077051eea47c51e8fe
Loran Raifi <loranraifi@gmail.com>
2025-05-16 19:59:33 +0200

    modification i158

--- app-raifi.py
+++ app-raifi.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Batman le bg x2 ++"
+    return "Batman le bg x2 ++++ trop bg de ouf"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
